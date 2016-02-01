f4e4a86e0f81bc3063d12a958d32e2724dd36d6d
Paul Fenwick <pjf@perltraining.com.au>
2016-02-01 14:30:56 +1100

    xmonad.hs: Fixup: Chat workspace now uses the chat layout

--- xmonad.hs
+++ xmonad.hs
@@ -172,7 +172,7 @@ gimpLayout = smartBorders(avoidStruts(ThreeColMid 1 (3/100) (3/4)))
 -- Here we combine our default layouts with our specific, workspace-locked
 -- layouts.
 myLayouts =
-  onWorkspace "7:Chat" chatLayout
+  onWorkspace "9:Chat" chatLayout
   $ onWorkspace "9:Pix" gimpLayout
   $ defaultLayouts
 

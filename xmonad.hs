d07f97ea87bf643a49944fbb00b92f96c76bff1f
Paul Fenwick <pjf@perltraining.com.au>
2016-07-17 11:40:51 -0400

    Remap media keys to be more user-friendly.
    The media keys on the direction pad no longer do what they say they'll
    do. Instead up/down do volume, rather than stop and pause. Those were
    silly bindings, and we don't have to do what the labels tell us to.

--- xmonad.hs
+++ xmonad.hs
@@ -218,10 +218,11 @@ myKeyBindings =
 
     -- I use Banshee as my music player. You may wish to bind the multimedia
     -- keys to something different.
-    , ((0, 0x1008FF14), spawn "banshee --toggle-playing")  -- Pause/Play
-    , ((0, 0x1008FF15), spawn "banshee --stop")            -- Stop
-    , ((0, 0x1008FF16), spawn "banshee --previous")        -- Prev
-    , ((0, 0x1008FF17), spawn "banshee --next")            -- Next
+    , ((0, 0x1008FF14), spawn "amixer -q set Master 10%-")      -- Fn-down
+    , ((0, 0x1008FF15), spawn "amixer -q set Master 10%+")      -- Fn-up
+    , ((0, 0x1008FF16), spawn "banshee --previous")             -- Fn-left
+    , ((0, 0x1008FF17), spawn "banshee --next")                 -- Fn-right
+    , ((myModMask, xK_space), spawn "banshee --toggle-playing") -- Super-Space
 
     -- P57W workarounds. The Fn-keys on my laptop do squat.
     , ((myModMask, xK_F3), spawn "xbacklight -dec 10")
